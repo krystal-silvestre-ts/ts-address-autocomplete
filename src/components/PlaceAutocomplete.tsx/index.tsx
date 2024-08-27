89e026f5a4b3b1baeeb57782a57a465607c84752
krystal-silvestre-ts <krystal.silvestre@trustingsocial.com>
2024-08-27 11:54:13 +0800

    Fix error message display

--- src/components/PlaceAutocomplete.tsx/index.tsx
+++ src/components/PlaceAutocomplete.tsx/index.tsx
@@ -1,4 +1,4 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import { useRef, useEffect, useState } from 'react';
 import { useMapsLibrary } from '@vis.gl/react-google-maps';
 import { Input } from '@mui/material';
@@ -7,11 +7,18 @@ interface Props {
   onPlaceSelect: (place: google.maps.places.PlaceResult) => void;
 }
 
+const ErrorMessage = styled.div`
+  color: red;
+  margin-top: 5px;
+  font-size: 12px;
+`;
+
 // This is an example of the classic "Place Autocomplete" widget.
 // https://developers.google.com/maps/documentation/javascript/place-autocomplete
 export const PlaceAutocomplete = ({ onPlaceSelect }: Props) => {
   const [placeAutocomplete, setPlaceAutocomplete] =
     useState<google.maps.places.Autocomplete | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
   const places = useMapsLibrary('places');
 
@@ -34,12 +41,16 @@ export const PlaceAutocomplete = ({ onPlaceSelect }: Props) => {
     placeAutocomplete.addListener('place_changed', () => {
       const place = placeAutocomplete.getPlace();
       if (!place.geometry) {
-        window.alert(`No details available for input: '${place.name}'`);
-        return;
+        setErrorMessage(`No details available for input: '${place.name}'`);
       }
       onPlaceSelect(place);
     });
   }, [onPlaceSelect, placeAutocomplete]);
 
-  return <Input inputRef={inputRef} placeholder='Enter Current Home Address' />;
+  return (
+    <>
+      <Input inputRef={inputRef} placeholder='Enter Current Home Address' />
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </>
+  );
 };
